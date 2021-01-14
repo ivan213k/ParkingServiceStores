d95a08a8c815aaa0a7ab780d4afb2145f09db7d2
Admin <Admin@DESKTOP-1RSB9I9>
2021-01-14 10:23:21 +0200

    Implemented application db context

--- ParkingServiceStores/Data/ApplicationDbContext.cs
+++ ParkingServiceStores/Data/ApplicationDbContext.cs
@@ -1,16 +1,40 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using ParkingServiceStores.Data.Models;
 
 namespace ParkingServiceStores.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<CarOwner> CarOwners { get; set; }
+        public virtual DbSet<JournalRecord> Journal { get; set; }
+        public virtual DbSet<Price> Prices { get; set; }
+        public virtual DbSet<Debt> Debts { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Car>()
+                .HasOne(c => c.Owner)
+                .WithOne(o => o.Car)
+                .HasForeignKey<CarOwner>(o => o.CarId);
+
+            builder.Entity<Car>()
+                .HasMany(c => c.JournalRecords)
+                .WithOne(r => r.Car)
+                .HasForeignKey(r => r.CarId);
+
+            builder.Entity<Car>()
+                .HasMany(c => c.Debts)
+                .WithOne(d => d.Car)
+                .HasForeignKey(d => d.CarId);
+
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ ParkingServiceStores/Data/Migrations/20210114081610_Implemented_DB_Structure.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingServiceStores.Data.Migrations
+{
+    public partial class Implemented_DB_Structure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlateNumber = table.Column<string>(nullable: false),
+                    Model = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(nullable: false),
+                    OneHourPrice = table.Column<decimal>(nullable: false),
+                    OneDayPrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarOwners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    CarId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarOwners", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarOwners_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Debts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarId = table.Column<int>(nullable: false),
+                    DebtAmount = table.Column<decimal>(nullable: false),
+                    IsPayed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Debts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Debts_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Journal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntryTime = table.Column<DateTime>(nullable: false),
+                    LeavingTime = table.Column<DateTime>(nullable: false),
+                    CarId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Journal", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Journal_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarOwners_CarId",
+                table: "CarOwners",
+                column: "CarId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debts_CarId",
+                table: "Debts",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journal_CarId",
+                table: "Journal",
+                column: "CarId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarOwners");
+
+            migrationBuilder.DropTable(
+                name: "Debts");
+
+            migrationBuilder.DropTable(
+                name: "Journal");
+
+            migrationBuilder.DropTable(
+                name: "Prices");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
