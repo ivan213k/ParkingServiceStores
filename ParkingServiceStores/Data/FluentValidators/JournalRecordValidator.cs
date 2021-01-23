ae2a58c56042449a0354fd1e5c6bfe9bf0fe13a7
Admin <Admin@DESKTOP-1RSB9I9>
2021-01-23 12:30:03 +0200

    Implemented fluent validation for price and journalRecord add, edit forms

---
+++ ParkingServiceStores/Data/FluentValidators/JournalRecordValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ParkingServiceStores.Data.Models;
+
+namespace ParkingServiceStores.Data.FluentValidators
+{
+    public class JournalRecordValidator : AbstractValidator<JournalRecord>
+    {
+        public JournalRecordValidator()
+        {
+            RuleFor(record => record.EntryTime).LessThan(r => r.LeavingTime).WithMessage("Entry time can not be more than Leaving time!");
+            RuleFor(record => record.LeavingTime).GreaterThan(r => r.EntryTime).WithMessage("Leaving time can not be less than Entry time!");
+        }
+    }
+}
---
+++ ParkingServiceStores/Data/FluentValidators/PriceValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using ParkingServiceStores.Data.Models;
+
+namespace ParkingServiceStores.Data.FluentValidators
+{
+    public class PriceValidator : AbstractValidator<Price>
+    {
+        public PriceValidator()
+        {
+            RuleFor(price => price.OneDayPrice).Must(p => p > 0).WithMessage("One day price should be more then 0");
+            RuleFor(price => price.OneHourPrice).Must(p => p > 0).WithMessage("One hour price should be more then 0");
+            RuleFor(price => price.Date).NotEmpty().WithMessage("Date of price required");
+        }
+    }
+}
